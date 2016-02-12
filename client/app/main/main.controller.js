9b53101e1be9a59cf987c3892c61e515b03d942a
Sean Nguyen <sean.nguyentrong@gmail.com>
2016-02-13 02:57:18 +0700

    Adjust nabber styles

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -18,6 +18,47 @@ function MainController($stateParams) {
       this.currentTab = 3;
       break;
   }
+
+  this.searchTextUniversity = '';
+  this.searchTextLocation = '';
+
+  this.universities = ['National University of Singapore', 
+                      'Nanyang Technological University', 
+                      'Singapore Management University', 
+                      'Singapore University of Technology and Design',
+                      'Singapore Institute of Technology',
+                      'SIM University']
+  this.locations = ['Serbia',
+                    'Seychelles',
+                    'Sierra Leone',
+                    'Singapore',
+                    'Slovakia',
+                    'Slovenia',
+                    'Solomon Islands',
+                    'Somalia',
+                    'South Africa']
+
+  this.queryUnivesity = function(query) {
+    query = query.toLowerCase();
+    var results = [];
+    for (var i = 0; i < this.universities.length; i++) {
+      var name = this.universities[i].toLowerCase();
+      if(name.indexOf(query) > -1)
+        results.push(this.universities[i]);
+    }
+    return results;
+  }
+
+  this.queryLocations = function(query) {
+    query = query.toLowerCase();
+    var results = [];
+    for (var i = 0; i < this.locations.length; i++) {
+      var name = this.locations[i].toLowerCase();
+      if(name.indexOf(query) > -1)
+        results.push(this.locations[i]);
+    }
+    return results;
+  }
 }
 
 angular.module('app')
