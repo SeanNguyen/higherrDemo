33504de2d1005ae39c31fa8b14ffcb9983703775
Sean Nguyen <sean.nguyentrong@gmail.com>
2016-02-16 00:48:24 +0700

    Update primary color, and navbar height

--- client/app/app.js
+++ client/app/app.js
@@ -9,9 +9,21 @@ angular.module('app', [
   'ui.bootstrap',
   'ngMaterial'
 ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, $mdThemingProvider) {
     $urlRouterProvider
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
+
+    // Extend the red theme with a few different colors
+    var theme = $mdThemingProvider.extendPalette('red', {
+      '500': '046B8B',
+      'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
+    });
+    // Register the new color palette map with the name <code>neonRed</code>
+    $mdThemingProvider.definePalette('theme', theme);
+    // Use that theme for the primary intentions
+    $mdThemingProvider.theme('default')
+      .primaryPalette('theme')
+
   });
