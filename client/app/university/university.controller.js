1a10632adc1b02fdd066d86776c6ec49a669e3e4
Sean Nguyen <sean.nguyentrong@gmail.com>
2016-02-19 23:40:33 +0700

    Adjust routing

--- client/app/university/university.controller.js
+++ client/app/university/university.controller.js
@@ -3,13 +3,21 @@
 (function() {
 
 angular.module('app')
-  .controller('UniversityController', ['$stateParams', '$timeout', UniversityController]);
+  .controller('UniversityController', ['$stateParams', '$timeout', '$state', UniversityController]);
 
-function UniversityController($stateParams, $timeout) {
+function UniversityController($stateParams, $timeout, $state) {
   this.universities = [{ name: 'University of Hong Kong ', photoUrl: '/assets/images/logo-hku.jpg', rating: 4 },
                       { name: 'Nanyang Technological University', photoUrl: '/assets/images/logo-ntu.jpg', rating: 3 },
                       { name: 'Singapore Management University', photoUrl: '/assets/images/logo-smu.jpg', rating: 5 },]
   this.init = function() {
+  }
+
+  this.id = $stateParams.id;
+
+  this.isAtState = function(stateName) {
+    if($state.current.name.indexOf(stateName) > -1)
+      return true;
+    return false;
   }
 }
 })();
